a7b723e7284c0f187672ed62b2d5817a82e7b5fe
santoskarolina <anna15soliveira@gmail.com>
2022-11-27 17:04:30 -0300

    📝 removing code

--- lib/main.dart
+++ lib/main.dart
@@ -11,13 +11,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       title: 'Movie App',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
